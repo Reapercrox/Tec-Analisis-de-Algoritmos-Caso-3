baacaf7aefc67b2cc4b0da5166ea878617afc478
RolbinM <rolbinmendezb@gmail.com>
2021-03-09 13:24:39 -0600

    Including memory calculation

--- Caso3C/sequentialSearch.c
+++ Caso3C/sequentialSearch.c
@@ -4,36 +4,41 @@
 #include <time.h>
 #include <stdbool.h>
 
-#define CANTIDAD 10000000
+#define CANTIDAD 200
+static int numArray[CANTIDAD];
+const int numBuscado = -1;
 
-void busqueda(long pNumArray[]);
+void busqueda(int pBuscado);
 
 int main(){
-    static long numArray[CANTIDAD];
-    long i;
+	int* pointerDir1 = &numArray;
+	int* pointerDir2 = &numBuscado;
+    int i;
 	double seconds;
-	srand(time(0));
     for(i = 0; i<CANTIDAD;i++){
-    	numArray[i] = rand();
+    	numArray[i] = 123;						// Se inserta solo el mismo numero positivo
 	}
 
 	clock_t tInicio = clock();					// Marca inicio del tiempo
-	
-	busqueda(numArray);
-
+	busqueda(numBuscado);						// Busqueda de un numero negativo para hacer que no lo encuentre
 	clock_t tFinal = clock();					// Marca final del tiempo
+
 	seconds = (double)(tFinal - tInicio)*1000000000 / CLOCKS_PER_SEC;
+	int memory = pointerDir1 - pointerDir2;
+
+	printf("Ejemplo de busqueda secuencial con %d elementos\n",CANTIDAD);
 	printf("El programa se ejecuto en: %f nanosegundos\n",seconds);
+	printf("La primera direccion de memoria es: %d\n",memory);
 }
 
-//Funcion principal
+//Funcion de busqueda secuencial
 //Se encarga de realizar una busqueda secuencial en un arreglo randomizado
-void busqueda(long pNumArray[]){
+void busqueda(int pBuscado){
 	int i;
 	bool estado = false;
 
 	for(i=0; i<CANTIDAD;i++){
-		if(pNumArray[i] == -1){//Se busca el -1 de manera que se de el peor caso siempre
+		if(pNumArray[i] == -1){					//Se busca el numero de manera que se de el peor caso siempre
 			estado = true;
 		}
 	}
